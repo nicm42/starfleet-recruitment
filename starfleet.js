89d74fce10a8a0b01e5329d310c1d9f8bb2743df
Nic Mayer <nicmayer42@gmail.com>
2019-05-26 17:01:52 +0100

    Got carousel basically working

--- starfleet.js
+++ starfleet.js
@@ -16,29 +16,54 @@ function hideMenu(){
 const leftArrow = document.getElementById('arrow-left');
 const rightArrow = document.getElementById('arrow-right');
 
+const command = document.getElementById('command');  //left-most career path
+const security = document.getElementById('security');  //right-most career path
+
+let carousel = document.getElementsByClassName('carousel');
+let pathOffset = [];
+for(var i=0; i<carousel.length; i++){
+	pathOffset[i] = 0;
+}
+
 leftArrow.addEventListener('click', moveLeft);
 rightArrow.addEventListener('click', moveRight);
 
-let carousel = document.getElementsByClassName('carousel');
-
 function moveLeft(){
-	for(var path of carousel){
-		//path.classList.add('moveLeft');
-		let pathOffset = offset(path);
-		newLeft = pathOffset.left - 235;
-		//console.log(newLeft);
-		//path.style.left = newLeft;
-		path.style.transform = "translateX("+newLeft+"px)";
+	//if Command is at position left <= 15 then disable left arrow
+	let commandPosition = offset(command);
+	let commandLeft = commandPosition.left;
+	//if command is visible, disable left arrow
+	if(isInViewport(command)){
+		leftArrow.removeEventListener('click',moveLeft);
+		leftArrow.style.cursor = 'auto';
+	}else{
+		for(var i=0; i<carousel.length; i++){
+			pathOffset[i] += 235;
+			carousel[i].style.left = pathOffset[i] + "px";
+		}		
+	}
+	//if security is invisible, enable right arrow
+	if(!isInViewport(security)){
+		rightArrow.addEventListener('click', moveRight);
+		rightArrow.style.cursor = 'pointer';
 	}
 }
 
 function moveRight(){
-	for(var path of carousel){
-		//path.classList.add('moveRight');
-		//let pathOffset = offset(path);
-		//newRight = pathOffset.left + 235;
-		//path.style.left = newRight;
-		path.style.transform = "translateX(235px)";
+	//if security is visible, disable right arrow
+	if(isInViewport(security)){
+		rightArrow.removeEventListener('click',moveLeft);
+		rightArrow.style.cursor = 'auto';
+	}else{
+		for(var i=0; i<carousel.length; i++){
+			pathOffset[i] -= 235;
+			carousel[i].style.left = pathOffset[i] + "px";
+		}
+	}
+	//if command is invisible, enable right arrow
+	if(!isInViewport(command)){
+		leftArrow.addEventListener('click', moveLeft);
+		leftArrow.style.cursor = 'pointer';
 	}
 }
 
@@ -48,3 +73,19 @@ function offset(el) {
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
+
+/*!
+ * Determine if an element is in the viewport
+ * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}    elem The element
+ * @return {Boolean}      Returns true if element is in the viewport
+ */
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
