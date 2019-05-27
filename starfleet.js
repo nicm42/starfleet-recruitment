5b9e0fc13bc9476ab573a97001eab39e4c0b266c
Nic Mayer <nicmayer42@gmail.com>
2019-05-27 19:36:31 +0100

    Fixed carousel so it only cares if elements are visible horizontally

--- starfleet.js
+++ starfleet.js
@@ -117,9 +117,9 @@ function offset(el) {
 var isInViewport = function (elem) {
 	var distance = elem.getBoundingClientRect();
 	return (
-		distance.top >= 0 &&
+		//distance.top >= 0 &&
 		distance.left >= 0 &&
-		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		//distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
 		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
 	);
 };
