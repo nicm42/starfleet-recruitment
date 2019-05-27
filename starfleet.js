da34d936806748ab2b8c392364e472efcea1509b
Nic Mayer <nicmayer42@gmail.com>
2019-05-27 14:01:02 +0100

    Added timeout to carousel

--- starfleet.js
+++ starfleet.js
@@ -29,17 +29,41 @@ leftArrow.addEventListener('click', moveLeft);
 rightArrow.addEventListener('click', moveRight);
 
 function moveLeft(){
+	//disable left arrow temporarily, so it's not possible to go mad clicking on it
+	leftArrow.removeEventListener('click',moveRight);
+	//re-enable it after transition has finished (it takes 1s)
+	setTimeout(enableLeftArrow, 1000);
 	if(!isInViewport(command)){
+		moveArrow(leftArrow);
 		for(var i=0; i<carousel.length; i++){
 			pathOffset[i] += 235;
 			carousel[i].style.left = pathOffset[i] + "px";
 		}
 	}
+}
+
+function moveRight(){
+	//disable right arrow temporarily, so it's not possible to go mad clicking on it
+	rightArrow.removeEventListener('click',moveRight);
+	//re-enable it after transition has finished (it takes 1s)
+	setTimeout(enableRightArrow, 1000);
+	if(!isInViewport(security)){
+		moveArrow(rightArrow);
+		for(var i=0; i<carousel.length; i++){
+			pathOffset[i] -= 235;
+			carousel[i].style.left = pathOffset[i] + "px";
+		}
+	}
+}
+
+function enableLeftArrow(){
 	//if command is now visible, disable left arrow
 	if(isInViewport(command)){
 		leftArrow.removeEventListener('click',moveLeft);
 		leftArrow.style.cursor = 'auto';
 		leftArrowImage.src = "./images/leftarrow-deactivated.png";
+	}else{
+		leftArrow.addEventListener('click',moveLeft);	
 	}
 	//if security is now invisible, enable right arrow
 	if(!isInViewport(security)){
@@ -49,20 +73,14 @@ function moveLeft(){
 	}
 }
 
-function moveRight(){
-	if(!isInViewport(security)){
-		for(var i=0; i<carousel.length; i++){
-			var forced = carousel[i].scrollLeft; // Forces a redraw - doesn't work!
-			pathOffset[i] -= 235;
-			carousel[i].style.left = pathOffset[i] + "px";
-		}
-	}
-	var forced = carousel[0].scrollLeft; // Forces a redraw - doesn't work!
+function enableRightArrow(){
 	//if security is now visible, disable right arrow
 	if(isInViewport(security)){
 		rightArrow.removeEventListener('click',moveRight);
 		rightArrow.style.cursor = 'auto';
 		rightArrowImage.src = "./images/rightarrow-deactivated.png";
+	}else{
+		rightArrow.addEventListener('click',moveRight);
 	}
 	//if command is now invisible, enable left arrow
 	if(!isInViewport(command)){
@@ -70,6 +88,17 @@ function moveRight(){
 		leftArrow.style.cursor = 'pointer';
 		leftArrowImage.src = "./images/leftarrow.png";
 	}
+}
+
+function moveArrow(arrow){
+	arrow.style.transform = "translateX(2px)";
+	arrow.style.transform = "translateY(2px)";
+	setTimeout(moveBack, 100, arrow);
+}
+
+function moveBack(arrow){
+	arrow.style.transform = "translateX(0px)";
+	arrow.style.transform = "translateY(0px)";
 }
 
 function offset(el) {
@@ -99,9 +128,14 @@ var isInViewport = function (elem) {
 const submit = document.getElementById('submit');
 const brocureForm = document.getElementById('brochure-form');
 const submittedForm = document.getElementById('submitted-form');
+const email = document.getElementById('email');
 submit.addEventListener('click', submitForm);
 
 function submitForm(){
-	brocureForm.style.visibility = "hidden";
-	submittedForm.style.visibility = "visible";
+	if(email.value === ""){
+		alert("Please enter your subspace address");
+	}else{
+		brocureForm.style.visibility = "hidden";
+		submittedForm.style.visibility = "visible";
+	}
 }
